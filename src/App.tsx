5f84e53e431d6284e96c1669f5731ab7897e8caf
Chaerin Jeon <chaerin0411@naver.com>
2022-11-10 20:28:54 +0900

    Init project

---
+++ src/App.tsx
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from 'react';
+
+function App() {
+  const [time, setTime] = useState(new Date());
+
+  const timer = setInterval(() => {
+    setTime(new Date());
+  }, 1000);
+
+  useEffect(() => {
+    return () => {
+      clearInterval(timer);
+    };
+  }, [timer]);
+
+  let isEven = true;
+  if(time.getTime() % 2 === 0) {
+    isEven = true;
+  } else {
+    isEven = false;
+  }
+
+  return (
+    <div>
+      <h3>현재시간 : {time.toLocaleTimeString()}</h3>
+      <h3>지금 시간은 홀일까요 짝일까요? = {isEven ? '짝이네' : '홀이네'}</h3>
+      <h3>자기소개</h3>
+      <h3>이름 : 전채린</h3>
+      <h3>학력</h3>
+      <>
+        <li>경인고등학교</li>
+        <li>동양미래대학교</li>
+      </>
+      <h3>경력</h3>
+      <>
+        <li>씨큐프라임 인턴 1개월</li>
+      </>
+    </div>
+  );
+}
+
+export default App;
